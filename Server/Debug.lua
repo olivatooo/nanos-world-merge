db17c60bc5a09ab37d5ba92e677b5343923ad061
olivato <olivatooo@gmail.com>
2025-02-10 20:08:50 +0000

    :bug: removes debug package

--- Server/Debug.lua
+++ Server/Debug.lua
@@ -1,9 +1,10 @@
-Events.SubscribeRemote("ReloadPackages", function()
-  Console.Log("Reloading Packages")
-  Events.BroadcastRemote("ServerLog", "Start reloading packages", "blue")
-  for k, v in pairs(Server.GetPackages(true)) do
-    Console.Log("Reloading Package: " .. v.name)
-    Chat.BroadcastMessage("Reloading Package: " .. v.name)
-    Server.ReloadPackage(v.name)
-  end
-end)
+-- REMOVES DEBUG
+-- Events.SubscribeRemote("ReloadPackages", function()
+--   Console.Log("Reloading Packages")
+--   Events.BroadcastRemote("ServerLog", "Start reloading packages", "blue")
+--   for k, v in pairs(Server.GetPackages(true)) do
+--     Console.Log("Reloading Package: " .. v.name)
+--     Chat.BroadcastMessage("Reloading Package: " .. v.name)
+--     Server.ReloadPackage(v.name)
+--   end
+-- end)
